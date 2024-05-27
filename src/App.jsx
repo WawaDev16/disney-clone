68519d15f88ba7e604a1d6b71a71c3df3771222d
WawaDev16 <wawa.dev@gmail.com>
2024-05-27 17:59:37 +0800

    setting up the disney logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 }
